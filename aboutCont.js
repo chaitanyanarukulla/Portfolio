331eab55029e4a65eba9d82258679000c1f2c526
Chaitanya.Narukulla <Chai@Ashleys-MacBook-Air.local>
2017-07-27 23:58:40 -0700

    refactor

---
+++ aboutCont.js
@@ -0,0 +1,12 @@
+'use strict';
+var app = app || {};
+
+(function(module) {
+  const aboutController = {};
+
+  aboutController.index = () => {
+
+  };
+
+  module.aboutController = aboutController;
+})(app);
--- app.js
+++ app.js
@@ -6,79 +6,34 @@ $(document).ready(function() {
       e.preventDefault();
       $('.list').slideToggle();
     });
-    // show and hide projects
-    $('#articles').hide();
-    $('.showpro').click(function() {
-      if ($('#articles').is(':hidden')) {
-        $('#articles').slideDown('slow');
-      } else {
-        $('#articles').hide();
-      }
-    });
-
-    $('#aboutMe').hide();
-    $('.aboutMe').click(function() {
-      if ($('#aboutMe').is(':hidden')) {
-        $('#aboutMe').slideDown('slow');
-      } else {
-        $('#aboutMe').hide();
-      }
-    });
   });
+  // show and hide Tabs
+  $('#articles').hide();
+  $('.showpro').click(() => $('#articles').toggle('slow'));
+
+  $('#aboutMe').hide();
+  $('.aboutMe').click(() => $('#aboutMe').toggle('slow'));
+
   $('#game').hide();
-  $('.game').click(function() {
-    if ($('#game').is(':hidden')) {
-      $('#game').slideDown('slow');
-    } else {
-      $('#game').hide();
-    }
-  });
-
-
-  // Constructor function
-
-  var projects = [];
-  // Projects.all = [];
-
-  function Project(rawDataObj) {
-    this.title = rawDataObj.title;
-    this.projectUrl = rawDataObj.projectUrl;
-    this.image = rawDataObj.image;
-    this.catagory = rawDataObj.catagory;
-    this.complatedOn = rawDataObj.complatedOn;
-  }
-  // grab the template  and compile
-  Project.prototype.toHtml = function() {
-    // var $newProject = $('myprojectsList-template').clone();
-
-    var myprojectsList = $('#myprojectsList-template').html();
-    var compiled = Handlebars.compile(myprojectsList);
-    $('#articles').append(compiled(this));
-    // return compiled(this);
-  };
-
-  rawData.forEach(function(projectObject) {
-    projects.push(new Project(projectObject));
-  });
-
-  projects.forEach(function(Project) {
-    $('#articles').append(Project.toHtml());
-  });
+  $('.game').click(() => $('#game').toggle('slow'));
 });
+// Constructor function
+var projects = [];
 
-Projects.fetchAll = function() {
-  if (localStorage.rawData) {
-    Projects.loadAll(JSON.parse(localStorage.rawData));
-  } else {
-
-    $.getJSON('rawData.jason')
-      .then(function(data) {
-        localStorage.setItem('rawData', JSON.stringify(data))
-        console.log('SUCCESS!' + data)
-        Projects.loadAll(JSON.parse(localStorage.rawData))
-      }, function(err) {
-        console.error('Error Has Occured' + err)
-      })
-  }
+function Project(rawDataObj) {
+  this.title = rawDataObj.title;
+  this.projectUrl = rawDataObj.projectUrl;
+  this.image = rawDataObj.image;
+  this.catagory = rawDataObj.catagory;
+  this.complatedOn = rawDataObj.complatedOn;
 }
-// Projects.fetchAll();
+// grab the template  and compile
+
+Project.prototype.toHtml = function() {
+  let compiled = Handlebars.compile($('#myprojectsList-template').html())
+  $('#articles').append(compiled(this));
+};
+
+rawData.forEach((projectObject) => projects.push(new Project(projectObject)));
+
+projects.forEach((Project) => $('#articles').append(Project.toHtml()));
